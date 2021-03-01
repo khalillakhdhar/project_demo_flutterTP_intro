cd61135684094ff1b6defd490009e71653d0375c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-01 19:27:34 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(new Container(
+      decoration: new BoxDecoration(color: Colors.blue),
+      child: new Center(
+        child: new Text(
+          "hello flutter",
+          textDirection: TextDirection.ltr,
+        ),
+      )));
 }
